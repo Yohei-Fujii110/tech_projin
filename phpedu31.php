bb7574d5bff2a4d725e7f0d7359db59826e3c803
Yohei-Fujii110 <155057244+Yohei-Fujii110@users.noreply.github.com>
2024-04-16 21:04:34 +0900

    phpedu31.phpファイルを追加

---
+++ phpedu31.php
@@ -0,0 +1,21 @@
+<?php
+function minus($x, $y = 30) {
+  return $x - $y;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>31問目</title>
+</head>
+<body>
+  <p>
+    <?php
+    echo minus(70, 35);
+    ?>
+  </p>
+</body>
+</html>
