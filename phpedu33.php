0dd4f575cae2994db8cfcde60640c1412bda6034
Yohei-Fujii110 <155057244+Yohei-Fujii110@users.noreply.github.com>
2024-04-16 23:14:09 +0900

    phpedu33.phpファイルを追加

---
+++ phpedu33.php
@@ -0,0 +1,40 @@
+<?php
+function division($x, $y = 0){
+  if($y == 0) {
+    return '0の除算はできません。';
+  } else {
+    return $x / $y;
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>33問目</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="page-header">
+      <h1><i class="fa fa-check-square"></i>PHP問題３３</h1>
+    </div>
+
+    <div class="jumbotron">
+      <h3>
+        <?php
+        echo division(40, 5);
+        ?>
+      </h3>
+    </div>
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+</body>
+</html>
